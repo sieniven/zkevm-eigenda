35b2aa6f74145f8b4dee861ca5bc577dd34af88f
Niven <sieniven@gmail.com>
2024-05-30 15:22:24 +0800

    Cleanup eigenda verifier contract

--- contracts/eigenda/src/EigenDAVerifier.sol
+++ contracts/eigenda/src/EigenDAVerifier.sol
@@ -6,16 +6,17 @@ import {EigenDARollupUtils} from "eigenda/libraries/EigenDARollupUtils.sol";
 import {IEigenDAServiceManager} from "eigenda/interfaces/IEigenDAServiceManager.sol";
 import {BN254} from "eigenlayer-middleware/libraries/BN254.sol";
 import "../interfaces/IDataAvailabilityProtocol.sol";
+import "../interfaces/IPolygonZkEVMVEtrogErrors.sol";
 
 /*
  * Contract responsible for on-chain data availability verificationof L2 batch data submitted to the EigenDA
  * layer using the blob verification proofs. The contract serves as an adaptor pattern for the existing DA
  * interface on the PolygonValidium data availability protocol to interact with the EigenDAServiceManager.
  */
-contract EigenDAVerifier is IDataAvailabilityProtocol {
-    // Name of the data availability protocol
-    string internal constant _PROTOCOL_NAME = "EigenDA";
-
+contract EigenDAVerifier is
+    IDataAvailabilityProtocol,
+    IPolygonZkEVMVEtrogErrors
+{
     /**
      * @notice Struct which will store the blob verification data
      * @param blobHeader stores the header of the blob containing the relevant attributes of the blob
@@ -27,15 +28,77 @@ contract EigenDAVerifier is IDataAvailabilityProtocol {
         EigenDARollupUtils.BlobVerificationProof blobVerificationProof;
     }
 
+    // Name of the data availability protocol
+    string internal constant _PROTOCOL_NAME = "EigenDA";
+
+    // Address of the EigenDA service manager
     IEigenDAServiceManager dataAvailabilityProtocol;
+
+    // Address that will be able to adjust contract parameters
+    address public admin;
+
+    // This account will be able to accept the admin role
+    address public pendingAdmin;
+
+    /**
+     * @dev Emitted when the admin updates the data availability protocol address
+     */
+    event SetDataAvailabilityProtocol(IEigenDAServiceManager newTrustedSequencer);
+
+    /**
+     * @dev Emitted when the admin starts the two-step transfer role setting a new pending admin
+     */
+    event TransferAdminRole(address newPendingAdmin);
+
+    /**
+     * @dev Emitted when the pending admin accepts the admin role
+     */
+    event AcceptAdminRole(address newAdmin);
 
     /**
      * @param _eigenDAServiceManager EigenDA service manager address
      */
-    constructor(IEigenDAServiceManager _eigenDAServiceManager) {
+    constructor(
+        address _admin,
+        IEigenDAServiceManager _eigenDAServiceManager
+    ) {
+        admin = _admin;
         dataAvailabilityProtocol = _eigenDAServiceManager;
     }
 
+    modifier onlyAdmin() {
+        if (admin != msg.sender) {
+            revert OnlyAdmin();
+        }
+        _;
+    }
+
+    /**
+     * @notice Verifies that the given blob verification proof has been signed by EigenDA operators and verified
+     * on-chain to be to be available
+     * @param data Byte array containing the abi-encoded EigenDA blob verification proof to be used for on-chain
+     * verification with the EigenDAServiceManager
+     */
+    function verifyMessage(bytes32, bytes calldata data) external view {
+        BlobData memory blob = decodeBlobData(data);
+        EigenDARollupUtils.verifyBlob(blob.blobHeader, dataAvailabilityProtocol, blob.blobVerificationProof);
+    }
+
+    /**
+     * @notice Return the protocol name
+     */
+    function getProcotolName() external pure override returns (string memory) {
+        return _PROTOCOL_NAME;
+    }
+
+    /////////////////////////////////////////////
+    // Decode data availability message functions
+    /////////////////////////////////////////////
+
+    /**
+     * @notice Decodes the data availaiblity message to the EigenDA blob data
+     * @param data The encoded data availability message bytes
+     */
     function decodeBlobData(bytes memory data) internal pure returns (BlobData memory blobData) {
         uint256 offset = 0;
 
@@ -176,21 +239,40 @@ contract EigenDAVerifier is IDataAvailabilityProtocol {
         return result;
     }
 
+    //////////////////
+    // Admin functions
+    //////////////////
     /**
-     * @notice Verifies that the given blob verification proof has been signed by EigenDA operators and verified
-     * on-chain to be to be available
-     * @param data Byte array containing the abi-encoded EigenDA blob verification proof to be used for on-chain
-     * verification with the EigenDAServiceManager
+     * @notice Allow the admin to set a new data availability protocol
+     * @param newDataAvailabilityProtocol Address of the new trusted sequencer
      */
-    function verifyMessage(bytes32, bytes calldata data) external view {
-        BlobData memory blob = decodeBlobData(data);
-        EigenDARollupUtils.verifyBlob(blob.blobHeader, dataAvailabilityProtocol, blob.blobVerificationProof);
+    function setDataAvailabilityProtocol(
+        IEigenDAServiceManager newDataAvailabilityProtocol
+    ) external onlyAdmin {
+        dataAvailabilityProtocol = newDataAvailabilityProtocol;
+
+        emit SetDataAvailabilityProtocol(newDataAvailabilityProtocol);
     }
 
     /**
-     * @notice Return the protocol name
+     * @notice Starts the admin role transfer
+     * This is a two step process, the pending admin must accepted to finalize the process
+     * @param newPendingAdmin Address of the new pending admin
      */
-    function getProcotolName() external pure override returns (string memory) {
-        return _PROTOCOL_NAME;
+    function transferAdminRole(address newPendingAdmin) external onlyAdmin {
+        pendingAdmin = newPendingAdmin;
+        emit TransferAdminRole(newPendingAdmin);
+    }
+
+    /**
+     * @notice Allow the current pending admin to accept the admin role
+     */
+    function acceptAdminRole() external {
+        if (pendingAdmin != msg.sender) {
+            revert OnlyPendingAdmin();
+        }
+
+        admin = pendingAdmin;
+        emit AcceptAdminRole(pendingAdmin);
     }
 }
